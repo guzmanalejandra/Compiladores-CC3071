77e70548cd98cf653e8f1af5663da60fee24e5a2
Alejandra Guzman <77361246+guzmanalejandra@users.noreply.github.com>
2023-02-23 11:18:19 -0600

    Add files via upload

---
+++ Laboratorio01-Compis/main.py
@@ -0,0 +1,43 @@
+from infixpost.postfix import *
+from AFN.afn import *
+from Graph.graph import graph
+from Simulaciones.simulaciones import simulationNFA
+
+if __name__ == "__main__":
+    correcta = False
+    while not correcta:
+        option = input("\nDesea: 1.Crear AFN  2.Salirn\n>>")
+        if not option == "2":
+            expresion = input ("\nIngrese la expresión, por favor: ")
+            if firstExpresion(expresion):
+                if option == "1":
+                    print("---------- CREACIÓN AFN  ----------")
+                    nuevaexpresion = computableExpresion(expresion)
+                    print("Expresion ingresada: ",expresion)
+                    print("Expresion entendible para computadora: ",nuevaexpresion)
+                    postfixexp = infixaPostfix(nuevaexpresion)
+                    print("Expresion en Postfix:",postfixexp)
+                    result = ThompsonAlgorithm(postfixexp)
+                    nfaDict = result.getDict()
+                    #print("Dict con el NFA resultante:\n",nfaDict)
+                    prueba = graph(postfixexp,result)
+                    transitions = prueba.createTransitions()
+                    prueba.graphic(transitions,"Thompson")
+                    s0 = result.getInitial()
+                    sf = result.getFinal()
+                    states = prueba.getStates()
+                    #print("Nodo inicial: ",s0,"\nNodo de aceptación/final: ",sf)
+                    alphabet = getAlphabet(expresion)
+                    dictTrans = result.getDict()
+                    simulation = True
+                    while simulation:
+                        segundaExpresion = input ("\n------------- Nueva Simulación -------------\nIngrese la expresión a evaluar, por favor:\n>> ")
+                        resultSimNFA = simulationNFA(dictTrans, s0, sf, segundaExpresion, alphabet)
+                        print("Resultado de la simulación AFN: ", resultSimNFA)
+                        option = input ("¿Desea realizar otra simulacion?\n1.Sí   2.No\n>> ")
+                        if option == "2":
+                            simulation = False
+ 
+        else: 
+            correcta = True
+            print("ADIOS, gracias por usarme :)")
